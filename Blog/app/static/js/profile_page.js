69d54fe5639ca5bb0c3b5b236bc771a906e96cf6
BrunosBastos <Brunosb@ua.pt>
2020-11-17 20:57:28 +0000

    profile e tals

---
+++ Blog/app/static/js/profile_page.js
@@ -0,0 +1,11 @@
+
+$("#edit_button").click(function (){
+    $(".edit-profile").show();
+    $(".profile-data").hide();
+    $("#edit_button").hide();
+    $("#save_profile_button").show();
+})
+
+
+
+
